9ecd4066ef7c778b3fcbba91b01066d75908aacd
Code7G <111247923+Code7G@users.noreply.github.com>
2022-08-20 16:22:14 +0200

    optimization change

--- login.py
+++ login.py
@@ -59,7 +59,7 @@ elif login == 'login_failed':
 # set your own countdown duration by changing the value of t in secundes
     t = 180
     countdown(t)
-#                                                  # 2
+#                                           # 2
 # after this you can add as many tries as you want by copying the code from #1 to #2 without the # line
     login = (password(ps=pas))
     if login == 'login_complete':
